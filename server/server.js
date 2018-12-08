1bb9c98ea5413c9ae8d20f219de2949a6a35a286
Obielum Godson <godsonobielum@yahoo.com>
2018-12-08 16:25:55 +0100

    Set environmental variables

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const moment = require('moment');
 const session = require('express-session');
 const flash = require('connect-flash');
 const mongoStore = require('connect-mongo')(session);
-const db = process.argv.includes('production') ? process.env.PRODUCTION_DB : process.env.DEV_DB;
+const db = process.env.NODE_ENV === 'production' ? process.env.PRODUCTION_DB : process.env.DEV_DB;
 const path = require('path');
 
 const User = require('./models/user_model');
