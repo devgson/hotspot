d020a3218d507780343c3c4b04d0e5e9c7205f69
Obielum Godson <godsonobielum@yahoo.com>
2018-12-20 19:51:36 +0100

    Added analytics code

---
+++ public/js/analytics.js
@@ -0,0 +1,28 @@
+var pageIndex = location.pathname.lastIndexOf("/");
+var pageToGet = location.pathname.slice(pageIndex);
+
+(function(i, s, o, g, r, a, m) {
+  i["GoogleAnalyticsObject"] = r;
+  (i[r] =
+    i[r] ||
+    function() {
+      (i[r].q = i[r].q || []).push(arguments);
+    }),
+    (i[r].l = 1 * new Date());
+  (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
+  a.async = 1;
+  a.src = g;
+  m.parentNode.insertBefore(a, m);
+})(
+  window,
+  document,
+  "script",
+  "https://www.google-analytics.com/analytics.js",
+  "ga"
+);
+
+ga("create", "UA-130675440-1", "auto");
+ga("send", {
+  hitType: "pageviews",
+  page: pageToGet
+});
