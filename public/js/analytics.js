f57e85231fe16b5e8981353404c1cb621115ccf4
daodubasit <daodubasitseyi@yahoo.co.uk>
2018-12-29 01:07:21 +0100

    deployment

--- public/js/analytics.js
+++ public/js/analytics.js
@@ -1,14 +1,14 @@
 var pageIndex = location.pathname.lastIndexOf("/");
 var pageToGet = location.pathname.slice(pageIndex);
 
-(function (i, s, o, g, r, a, m) {
+(function(i, s, o, g, r, a, m) {
   i["GoogleAnalyticsObject"] = r;
   (i[r] =
     i[r] ||
-    function () {
+    function() {
       (i[r].q = i[r].q || []).push(arguments);
     }),
-  (i[r].l = 1 * new Date());
+    (i[r].l = 1 * new Date());
   (a = s.createElement(o)), (m = s.getElementsByTagName(o)[0]);
   a.async = 1;
   a.src = g;
